f4ea2c8fb3876c2f22bb1057251a2363afe9dd67
Petrik Diak <diak@petrik.hu>
2024-04-23 13:34:24 +0200

    a

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCarDto } from './dto/create-car.dto';
-import { UpdateCarDto } from './dto/update-car.dto';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
@@ -30,17 +29,5 @@ export class CarsService {
         updated_at : false
       }
     });
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} car`;
-  }
-
-  update(id: number, updateCarDto: UpdateCarDto) {
-    return `This action updates a #${id} car`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} car`;
   }
 }
